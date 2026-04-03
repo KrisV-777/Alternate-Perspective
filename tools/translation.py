f1e8266e40214eb0831aa0cd9c942d6b688ae5ef
Kris <kris.vlntn@gmail.com>
2026-04-03 13:46:18 +0200

    update translation.py

--- tools/translation.py
+++ tools/translation.py
@@ -1,4 +1,5 @@
 import shutil
+import sys
 from os import listdir
 from os.path import isfile, join
 
@@ -7,7 +8,7 @@ f_english = [f for f in listdir(path) if isfile(join(path, f)) and f.endswith("E
 
 if len(f_english) < 1:
   print("Missing Translation_ENGLISH.txt in directory")
-  exit()
+  sys.exit(1)
 
 f_raw = f_english[0].replace("ENGLISH.txt", "")
 
@@ -31,8 +32,13 @@ languages = [
 ]
 
 en_path = join(path, f_english[0])
+en_contents = open(en_path, "rb").read()
+
 for l in languages:
   new_path = join(path, f_raw + l + ".txt")
+  if isfile(new_path) and open(new_path, "rb").read() == en_contents:
+    print(f"{new_path} is already up to date")
+    continue
   shutil.copyfile(en_path, new_path)
 
 print("Done")
